4e8141be4e911605cce0f1b904fee78f546abf8b
KeithWilliamsGMIT <G00324844@gmit.ie>
2017-10-26 17:25:33 +0100

    Extracted hardcoded strings from XAML to resource file.

---
+++ DigiReceipt/DigiReceipt/DigiReceipt/TranslateExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DigiReceipt
+{
+    /// <summary>
+    /// A markup extension that exposes the RESX resources to XAML.
+    /// Adapted from https://mindofai.github.io/Implementing-Localization-with-Xamarin.Forms/.
+    /// </summary>
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        const string ResourceId = "DigiReceipt.Resx.AppResources";
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return null;
+
+            ResourceManager resourceManager = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
+
+            return resourceManager.GetString(Text, CultureInfo.CurrentCulture);
+        }
+    }
+}
